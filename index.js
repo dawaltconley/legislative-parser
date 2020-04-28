515765e2d3f018fd4b2f7c98cf4f2de62de4ec7a
Dylan Awalt-Conley <web@dylan.ac>
2020-04-28 19:02:10 -0400

    updated to use __dirname for loading grammar file, to work in node_modules folder

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
+const path = require('path')
 const peg = require('pegjs')
 
-const grammar = fs.readFileSync('./grammar.pegjs')
+const grammar = fs.readFileSync(path.join(__dirname, 'grammar.pegjs'))
 const parser = peg.generate(grammar.toString())
 
 module.exports = ref => parser.parse(ref)
